274851b5d12a4a6526a76c087550341504bf8e42
Jan Scholtyssek <jan@janscholtyssek.dk>
2022-08-31 17:30:56 +0200

    Changed layout of HorizontalLineWithText

--- components/HorizontalLineWithText.tsx
+++ components/HorizontalLineWithText.tsx
@@ -1,13 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+
 interface HorizontalLineWithTextProps {
     text: string;
+    variant?: string;
 }
 
 export const HorizontalLineWithText = (props: HorizontalLineWithTextProps) => {
 
     return (
-        <div className="timelineSeparator">
-            <span className="text-secondary">{props.text}</span>
-        </div>
+        <Row className="align-items-center my-1">
+            <Col className="pe-0"><div className="horizontal-line" /></Col>
+            <Col xs="auto"><span className={props.variant ? `text-${props.variant}` : undefined}>{props.text}</span></Col>
+            <Col className="ps-0"><div className="horizontal-line" /></Col>
+        </Row>
     );
 
 }
