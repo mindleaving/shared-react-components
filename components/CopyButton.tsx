0abcc1b9e7c85877f1d3ba975a53d4afea8ce44d
Jan Scholtyssek <jan@janscholtyssek.dk>
2022-11-01 12:13:59 +0100

    Added xs-version of CopyButton

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -4,7 +4,7 @@ import { resolveText } from '../helpers/Globalizer';
 
 interface CopyButtonProps {
     value: string;
-    size?: "sm" | "lg";
+    size?: "xs" | "sm" | "lg";
     className?: string;
 }
 
@@ -16,6 +16,24 @@ export const CopyButton = (props: CopyButtonProps) => {
         navigator.clipboard.writeText(props.value);
         setIsCopied(true);
         setTimeout(() => setIsCopied(false), 5000);
+    }
+
+    if(props.size === 'xs') {
+        return (
+            <span
+                className={`${props.className ?? ''} ${isCopied ? 'text-success' : ''}`}
+            >
+                <i
+                    className={`fa fa-files-o clickable`}
+                    onClick={copyToClipboard}
+                />
+                {isCopied 
+                ? <span className='ms-2'>
+                    {resolveText("Copied!")}
+                </span>
+                : null}
+            </span>
+        );
     }
 
     return (
