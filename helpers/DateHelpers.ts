b0636957fbcce10c3ed0a882e3a5b9b0f84f754a
Jan Scholtyssek <jan@janscholtyssek.dk>
2022-11-08 13:32:14 +0100

    Added isBirthday

---
+++ helpers/DateHelpers.ts
@@ -0,0 +1,16 @@
+import { getMonth, getDate, getYear, isSameDay, isLeapYear } from "date-fns";
+
+export const isBirthday = (date: Date, birthDate: Date) => {
+    const isSameDateAndMonth = getMonth(date) === getMonth(birthDate) && getDate(date) === getDate(birthDate);
+    if(isSameDateAndMonth) {
+        return true;
+    }
+    // Check for birthday on 29th of February which only exists in leap years
+    // If current year is not a leap year, report birthday on 1st of March
+    // Beware JS quirk: months are zero-indexed -> 0 = January, 1 = February,...
+    const is29thOfFebruary = getMonth(birthDate) === 1 && getDate(birthDate) === 29;
+    if(is29thOfFebruary && !isLeapYear(date)) {
+        return isSameDay(date, new Date(getYear(date), 2, 1));
+    }
+    return false;
+}
