bca061fcb9e49354449a17390a06b64e7d2ae1f8
Jan Scholtyssek <jan@janscholtyssek.dk>
2023-01-25 22:13:49 +0100

    Added item selector to toDictionary

--- helpers/Transformations.ts
+++ helpers/Transformations.ts
@@ -1,3 +1,12 @@
-export const toDictionary = <T extends unknown>(items: T[], keySelector: (item: T) => string) => {
-    return Object.assign({}, ...items.map(item => ({ [keySelector(item)]: item})));
+export const toDictionary = <T extends unknown, TOut extends unknown>(
+    items: T[], 
+    keySelector: (item: T) => string, 
+    itemSelector?: (item: T) => TOut)
+    : { [key:string]: TOut } => {
+    return Object.assign(
+        {}, 
+        ...items.map(item => ({ 
+            [keySelector(item)]: itemSelector ? itemSelector(item) : item
+        }))
+    );
 }
