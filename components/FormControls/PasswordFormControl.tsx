b0e5e682e0f399c39de3cfb5964f0afb45095557
Jan Scholtyssek <jan@janscholtyssek.dk>
2023-04-16 16:17:34 +0200

    Added PasswordFormControl

---
+++ components/FormControls/PasswordFormControl.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Button, FormControl, FormControlProps, InputGroup } from 'react-bootstrap';
+
+interface PasswordFormControlProps extends FormControlProps {
+    required?: boolean;
+    minLength?: number;
+    name?: string;
+}
+
+export const PasswordFormControl = (props: PasswordFormControlProps) => {
+
+    const [ showPassword, setShowPassword ] = useState<boolean>();
+
+    return (
+        <InputGroup>
+            <FormControl
+                type={showPassword ? 'text' : 'password'}
+                {...props}
+            />
+            <Button
+                variant={showPassword ? 'outline-danger' : 'outline-success'}
+                onClick={() => setShowPassword(state => !state)}
+            >
+                <i className={showPassword ? 'fa fa-eye-slash red' : 'fa fa-eye green'} />
+            </Button>
+        </InputGroup>
+    );
+
+}
+export default PasswordFormControl;
