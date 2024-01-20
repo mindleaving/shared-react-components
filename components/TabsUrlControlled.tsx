c56838bc9b2ddece17d76879d67297b9532107ff
Jan Scholtyssek <jan@janscholtyssek.dk>
2024-01-20 19:52:28 +0100

    Using setSearchParams in TabsUrlControlled instead of navigate

--- components/TabsUrlControlled.tsx
+++ components/TabsUrlControlled.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useEffect, useState } from "react";
+import { PropsWithChildren, useEffect, useMemo, useState } from "react";
 import { Tabs } from "react-bootstrap";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
@@ -17,7 +17,7 @@ export const TabsUrlControlled = (props: PropsWithChildren<TabsUrlControlledProp
 
     const { urlParameterName, validTabs, urlBuilder, id, className, mountOnEnter, unmountOnExit } = props;
 
-    const [ q ] = useSearchParams();
+    const [ q, setSearchParams ] = useSearchParams();
     const tabFromUrl = q.get(urlParameterName ?? defaultUrlParameterName)?.toLowerCase();
     const [ selectedTab, setSelectedTab ] = useState<string>(tabFromUrl && validTabs.includes(tabFromUrl) ? tabFromUrl : validTabs[0]);
     const navigate = useNavigate();
@@ -29,12 +29,23 @@ export const TabsUrlControlled = (props: PropsWithChildren<TabsUrlControlledProp
         }
     }, [ q, validTabs ]);
 
+    const changeTab = useMemo(() => (key: string) => {
+        if(urlBuilder) {
+            navigate(urlBuilder(key));
+            return;
+        }
+        setSearchParams(state => {
+            state.set(urlParameterName ?? defaultUrlParameterName, key);
+            return state;
+        });
+    }, [ urlBuilder, urlParameterName ]);
+
     return (
         <Tabs
             id={id}
             className={className}
             activeKey={selectedTab} 
-            onSelect={(key:any) => navigate(urlBuilder ? urlBuilder(key) : `${window.location.pathname}?tab=${key}`)}
+            onSelect={(key:any) => changeTab(key)}
             mountOnEnter={mountOnEnter}
             unmountOnExit={unmountOnExit}
         >
