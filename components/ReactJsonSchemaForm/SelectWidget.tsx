a7a90a66c3643dec78f00b58046a1f4b618b17da
Jan Scholtyssek <jan@janscholtyssek.dk>
2024-03-31 15:03:22 +0200

    Fixed mismatch between shown value and actual value in SelectWidget

--- components/ReactJsonSchemaForm/SelectWidget.tsx
+++ components/ReactJsonSchemaForm/SelectWidget.tsx
@@ -1,16 +1,33 @@
-import { WidgetProps } from '@rjsf/utils';
+import { GlobalUISchemaOptions, WidgetProps } from '@rjsf/utils';
 import { FormControl } from 'react-bootstrap';
 import { resolveText } from '../../helpers/Globalizer';
+import { useEffect, useMemo } from 'react';
 
+export interface SelectWidgetOptions extends GlobalUISchemaOptions {
+    defaultValue?: string;
+}
 export const SelectWidget = (props: WidgetProps) => {
+
+    const uiSchema = props.uiSchema;
+    const options = uiSchema?.['ui:options'] as SelectWidgetOptions;
+    const value = useMemo(() => props.options.enumOptions && !props.options.enumOptions.some(x => x.value === props.value)
+        ? options?.defaultValue ?? ''
+        : props.value ?? ''
+    , [ props.value, props.options.enumOptions, options]);
+
+    useEffect(() => {
+        if(props.value !== value) {
+            props.onChange(value);
+        }
+    }, []);
 
     return (
         <FormControl
             as="select"
-            value={props.value ?? ''}
+            value={value}
             onChange={e => props.onChange(e.target.value)}
         >
-            <option value='' disabled>{resolveText("PleaseSelect...")}</option>
+            <option value=''>{resolveText("PleaseSelect...")}</option>
             {(props.options.enumOptions ?? []).map(x => (
                 <option key={x.value} value={x.value}>{x.label}</option>
             ))}
