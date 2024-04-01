9aa41ca7f47e78ccc236c89127996acadbd61eb5
Jan Scholtyssek <jan@janscholtyssek.dk>
2024-04-01 19:48:34 +0200

    Added "Now"-button to DateFormControl

--- components/FormControls/DateFormControl.tsx
+++ components/FormControls/DateFormControl.tsx
@@ -1,5 +1,7 @@
 import Flatpickr from 'react-flatpickr';
 import { toDateOnly } from '../../helpers/DateHelpers';
+import { Row, Col, Button } from 'react-bootstrap';
+import { resolveText } from '../../helpers/Globalizer';
 
 interface DateFormControlProps {
     id?: string;
@@ -14,27 +16,44 @@ interface DateFormControlProps {
 export const DateFormControl = (props: DateFormControlProps) => {
 
     return (
-        <Flatpickr
-            options={{
-                allowInput: true,
-                enableTime: props.enableTime,
-                time_24hr: true,
-                mode: 'single'
-            }}
-            id={props.id}
-            name={props.name}
-            className="form-control"
-            required={props.required}
-            disabled={props.disabled}
-            value={props.value}
-            onChange={(dates) => {
-                if(dates.length > 0) {
-                    props.onChange(props.enableTime ? dates[0].toISOString() as any : toDateOnly(dates[0])); 
-                } else {
-                    props.onChange(undefined);
-                }
-            }} 
-        />
-    );
+    <Row>
+        <Col>
+            <Flatpickr
+                options={{
+                    allowInput: true,
+                    enableTime: props.enableTime,
+                    time_24hr: true,
+                    mode: 'single'
+                }}
+                id={props.id}
+                name={props.name}
+                className="form-control"
+                required={props.required}
+                disabled={props.disabled}
+                value={props.value}
+                onChange={(dates) => {
+                    if(dates.length > 0) {
+                        props.onChange(props.enableTime ? dates[0].toISOString() as any : toDateOnly(dates[0])); 
+                    } else {
+                        props.onChange(undefined);
+                    }
+                }} 
+            />
+        </Col>
+        <Col xs="auto">
+            <Button
+                onClick={() => {
+                    const now = new Date();
+                    if(props.enableTime) {
+                        props.onChange(now.toISOString() as any);
+                    } else {
+                        props.onChange(toDateOnly(now) as any);
+                    }
+                }}
+            >
+                {props.enableTime ? resolveText("Now") : resolveText("Today")}
+            </Button>
+        </Col>
+    </Row>);
 
 }
