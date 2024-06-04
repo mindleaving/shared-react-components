ab4dce0f9d494ca936f8959994769bfa02dedf19
Jan Scholtyssek <jan@janscholtyssek.dk>
2024-06-05 01:55:36 +0200

    Added xs-version of EditLinkButton

--- components/EditLinkButton.tsx
+++ components/EditLinkButton.tsx
@@ -5,15 +5,29 @@ import { resolveText } from '../helpers/Globalizer';
 interface EditLinkButtonProps {
     onClick: () => void;
     className?: string;
+    size?: 'xs';
 }
 
 export const EditLinkButton = (props: EditLinkButtonProps) => {
 
+    const { onClick, className, size } = props;
+
+    if(size === 'xs') {
+        const cssClasses = [ 'fa', 'fa-edit', 'clickable' ];
+        if(className) {
+            cssClasses.push(className);
+        }
+        return (<i
+            className={cssClasses.join(' ')}
+            onClick={onClick}
+        />);
+    }
+
     return (
         <Button 
             variant="link" 
-            onClick={props.onClick}
-            className={props.className}
+            onClick={onClick}
+            className={className}
         >
             {resolveText('Edit...')}
         </Button>
