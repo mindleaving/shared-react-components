debc599ab4edfa24d5c5d3dec608ea12e360de6a
Jan Scholtyssek <jan@janscholtyssek.dk>
2025-03-20 22:09:11 +0100

    Added className to LoadingAlert

--- components/LoadingAlert.tsx
+++ components/LoadingAlert.tsx
@@ -4,6 +4,7 @@ import { resolveText } from '../helpers/Globalizer';
 
 interface LoadingAlertProps {
     variant?: string;
+    className?: string;
 }
 
 export const LoadingAlert = (props: LoadingAlertProps) => {
@@ -11,6 +12,7 @@ export const LoadingAlert = (props: LoadingAlertProps) => {
     return (
         <Alert
             variant={props.variant ?? 'info'}
+            className={props.className}
         >
             {resolveText("Loading...")}
         </Alert>
