f9819d0fc375639170e88639ac7b6e91107428d9
Jan Scholtyssek <jan@janscholtyssek.dk>
2026-01-31 11:53:25 +0100

    Restyled DateSelectionRow

--- components/DateSelectionRow.tsx
+++ components/DateSelectionRow.tsx
@@ -1,23 +1,28 @@
 import { addDays } from 'date-fns';
 import { Row, Col, Button } from 'react-bootstrap';
 import { DateFormControl } from './FormControls/DateFormControl';
+import { PropsWithChildren } from 'react';
 
-interface DateSelectionRowProps {
+interface DateSelectionRowProps extends PropsWithChildren {
     date: string;
     onChange: (date: string) => void;
+    size?: 'sm' | 'lg';
 }
 export const DateSelectionRow = (props: DateSelectionRowProps) => {
-    const { date: selectedDate, onChange } = props;
-    return (<Row>
-        <Col />
-        <Col xs="auto">
+
+    const { date: selectedDate, onChange, size } = props;
+
+    return (<Row className='align-items-center'>
+        <Col>{props.children}</Col>
+        <Col xs="auto" className='pe-1'>
             <Button
                 onClick={() => onChange(addDays(new Date(selectedDate), -1).toISOString())}
+                size={size}
             >
                 &lt;
             </Button>
         </Col>
-        <Col xs={4} md={3} lg={2}>
+        <Col xs="auto" style={{ maxWidth: '240px' }} className='px-1'>
             <DateFormControl
                 value={selectedDate}
                 onChange={date => {
@@ -25,16 +30,17 @@ export const DateSelectionRow = (props: DateSelectionRowProps) => {
                         onChange(date);
                     }
                 }}
+                size={size}
             />
         </Col>
-        <Col xs="auto">
+        <Col xs="auto" className='ps-1'>
             <Button
                 onClick={() => onChange(addDays(new Date(selectedDate), 1).toISOString())}
+                size={size}
             >
                 &gt;
             </Button>
         </Col>
-        <Col />
     </Row>);
 }
 export default DateSelectionRow;
