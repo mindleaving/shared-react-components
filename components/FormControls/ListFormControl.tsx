53fcf268112b2957c251f35aef848eb804541e69
Jan Scholtyssek <jan@janscholtyssek.dk>
2026-06-27 18:56:44 +0200

    Added optional "Add"-button to ListFormControl

--- components/FormControls/ListFormControl.tsx
+++ components/FormControls/ListFormControl.tsx
@@ -6,16 +6,26 @@ interface ListFormControlProps<T> {
     idFunc: (item: T) => string;
     displayFunc: (item: T) => string | ReactNode;
     removeItem: (item: T) => void;
+    showAddButton?: boolean;
+    onAdd: () => void;
 }
 
 export const ListFormControl = <T,>(props: ListFormControlProps<T>) => {
+
+    const { items, idFunc, displayFunc, removeItem, showAddButton, onAdd } = props;
+
     return (
-        <div className="listFormControl">
-            {props.items.map(item => 
-                <Badge className="itemListBadge" bg="primary" key={props.idFunc(item)}>
-                    {props.displayFunc(item)} <Badge pill bg="danger" onClick={() => props.removeItem(item)}>X</Badge> 
+        <div className="list-form-control">
+            {items.map(item => 
+                <Badge className="itemListBadge" bg="primary" key={idFunc(item)}>
+                    {displayFunc(item)} <Badge pill bg="danger" onClick={() => removeItem(item)}>X</Badge> 
                 </Badge>
             )}
+            {showAddButton
+            ? <i
+                onClick={onAdd}
+                className='fa fa-plus green clickable ms-2'
+            /> : null}
         </div>
     );
 }
