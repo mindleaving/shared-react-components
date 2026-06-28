24d8107ae4c2de5ffbec0699375160d72e6294c1
Jan Scholtyssek <jan@janscholtyssek.dk>
2026-06-28 22:18:38 +0200

    Added fixInvalidUUID

--- helpers/uuid.ts
+++ helpers/uuid.ts
@@ -1,2 +1,8 @@
 export const uuid = () => crypto.randomUUID();
 export const EmptyUUID = '00000000-0000-0000-0000-000000000000';
+export const fixInvalidUUID = (id: string | undefined) => {
+    if(!id || id === EmptyUUID) {
+        return uuid();
+    }
+    return id;
+}
